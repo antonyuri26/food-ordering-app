23a01fb9fffafd17fe9225164b5319d3eb5012cc
Antonio Souza <antonyuri26+it@gmail.com>
2023-02-08 12:16:52 +1100

    create initial folder structure and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
+import Fonts from "./theme/Fonts";
+import theme from "./theme/theme";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+import HomePage from "./components/homePage/HomePage";
+import MenuPage from "./components/menuPage/MenuPage";
+import About from "./components/aboutPage/About";
+import Contact from "./components/contactPage/Contact";
+import ErrorPage from "./components/errorPage/ErrorPage";
+import RootLayout from "./components/layout/RootLayout";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+//creating routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      { path: "/menu", element: <MenuPage /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ src/components/menuPage/MenuPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuPage = () => {
+  return <h1>Menu Page</h1>;
+};
+
+export default MenuPage;
